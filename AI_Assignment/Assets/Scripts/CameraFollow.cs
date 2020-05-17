c6a7a97caa00d61405839ce62dd72bb878afc90b
Marcin7373 <skwarek.marcin@mydit.ie>
2020-05-17 20:42:43 +0100

    Added attackjet state machine, offset pursue, oject avoisance, colliders, multiple cameras, and manager for deaths and cameras

---
+++ AI_Assignment/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform[] cameraTarget;
+    public float smoothing = 2.0f;
+    private int curTarget = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            curTarget = 1;
+        }else if (Input.GetKeyDown("2"))
+        {
+            curTarget = 2;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, cameraTarget[curTarget].position, Time.deltaTime * smoothing);
+        transform.LookAt(cameraTarget[curTarget]);
+    }
+}
---
+++ AI_Assignment/Assets/Scripts/Manager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject[] cameras;
+    public AttackJet[] attackJets;
+    private int curTarget = 0, leader = 0;
+    public float checkInterval = 1f;
+
+    void Start()
+    {
+        attackJets[leader].lead = true;
+        StartCoroutine(CheckJetStatus());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            cameras[curTarget].SetActive(false);
+            curTarget = 1;
+            cameras[curTarget].SetActive(true);
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            cameras[curTarget].SetActive(false);
+            curTarget = 2;
+            cameras[curTarget].SetActive(true);
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            cameras[curTarget].SetActive(false);
+            curTarget = 3;
+            cameras[curTarget].SetActive(true);
+        }
+    }
+
+    IEnumerator CheckJetStatus()
+    {
+        while (true)
+        {
+            if (attackJets[leader].dead == true)
+            {
+                attackJets[leader].lead = false;
+                leader++;
+            }
+            else
+            {
+                attackJets[leader].lead = true;
+            }
+            yield return new WaitForSeconds(checkInterval);
+        }
+    }
+}
